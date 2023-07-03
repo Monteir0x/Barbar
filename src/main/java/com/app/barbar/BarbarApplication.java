67bfe442ab84e246f65f4559c7a7d22a6490c433
Matheus Monteiro <monteiro@desktop-home.home>
2023-07-03 09:21:25 -0300

    initial commit

---
+++ src/main/java/com/app/barbar/BarbarApplication.java
@@ -0,0 +1,13 @@
+package com.app.barbar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BarbarApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BarbarApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/app/barbar/BarbarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.barbar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BarbarApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
